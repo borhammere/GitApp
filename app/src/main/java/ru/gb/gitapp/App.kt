598ff841938b7e1091fdb9ac7841a7d0ce7b1025
borhammere <borhammere@gmail.com>
2022-06-30 20:25:13 +0300

    Base DI implementation

--- app/src/main/java/ru/gb/gitapp/App.kt
+++ app/src/main/java/ru/gb/gitapp/App.kt
@@ -3,12 +3,25 @@ package ru.gb.gitapp
 import android.app.Application
 import android.content.Context
 import androidx.fragment.app.Fragment
-import ru.gb.gitapp.data.FakeUsersRepoImpl
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.gb.gitapp.data.retrofit.GithubApi
 import ru.gb.gitapp.data.retrofit.RetrofitUsersRepoImpl
 import ru.gb.gitapp.domain.repos.UsersRepo
 
 class App : Application() {
-    val usersRepo: UsersRepo by lazy { RetrofitUsersRepoImpl() }
+    private val baseUrl = "https://api.github.com/"
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .build()
+    }
+    private val api: GithubApi by lazy { retrofit.create(GithubApi::class.java) }
+
+    val usersRepo: UsersRepo by lazy { RetrofitUsersRepoImpl(api) }
 }
 
 val Context.app: App get() = applicationContext as App
